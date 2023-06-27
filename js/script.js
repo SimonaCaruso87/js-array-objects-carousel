d3e6ee36960b406bd5ca66557b3863a73b1316ee
SimonaCaruso87 <caruso.simona87@gmail.com>
2023-06-27 16:02:35 +0200

    Update img

--- js/script.js
+++ js/script.js
@@ -19,26 +19,30 @@ Aggiungere bottoni di start/stop e di inversione del meccanismo di autoplay.*/
 
 const imagesArray = [
     {
-        image: 'img/01.webp',
+        image: '05.png',
         title: 'Marvel\'s Spiderman Miles Morale',
         text: 'Experience the rise of Miles Morales as the new hero masters incredible, explosive new powers to become his own Spider-Man.',
-    }, {
-        image: 'img/02.webp',
+    }, 
+    {
+        image: '02.jpeg',
         title: 'Ratchet & Clank: Rift Apart',
         text: 'Go dimension-hopping with Ratchet and Clank as they take on an evil emperor from another reality.',
-    }, {
-        image: 'img/03.webp',
+    }, 
+    {
+        image: '03.jpeg',
         title: 'Fortnite',
         text: "Grab all of your friends and drop into Epic Games Fortnite, a massive 100 - player face - off that combines looting, crafting, shootouts and chaos.",
-    }, {
-        image: 'img/04.webp',
+    }, 
+    {
+        image: '04.jpg',
         title: 'Stray',
         text: 'Lost, injured and alone, a stray cat must untangle an ancient mystery to escape a long-forgotten city',
-    }, {
-        image: 'img/05.webp',
+    }, 
+    {
+        image: '01.jpg',
         title: "Marvel's Avengers",
         text: 'Marvel\'s Avengers is an epic, third-person, action-adventure game that combines an original, cinematic story with single-player and co-operative gameplay.',
-    }
+    },
 ];
 
 let item_content = '';
